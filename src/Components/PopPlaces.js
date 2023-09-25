4aa0daece618a1a86e927253febc361231f3db6c
Nirbhay2002 <110183517+Nirbhay2002@users.noreply.github.com>
2023-09-25 23:36:58 +0530

    node modules added and tourist database added

--- src/Components/PopPlaces.js
+++ src/Components/PopPlaces.js
@@ -1,29 +1,46 @@
+import { Link } from 'react-router-dom';
+import TourPlaceData from '../TourPlaceData';
+import React from 'react';
+import TouristPlace from './TouristPlace';
+
 function PopPlaces() {
+
+    const [siteData, setSiteData] = React.useState(TourPlaceData);
+
+    const displaySite = siteData.map(site =>{
+        return  <div class = "popular-item shadow">
+            <img src = {site.url} alt = "" />
+            <div><i class = "fas fa-map-marker-alt"></i>
+                <span>{site.name}</span>
+                <ul class = "rating flex">
+                    {/* <li><i class = "fas fa-star"></i></li>
+                    <li><i class = "fas fa-star"></i></li>
+                    <li><i class = "fas fa-star"></i></li>
+                    <li><i class = "fas fa-star"></i></li> */}
+                    <li><i class = "fas fa-star-half-alt"></i></li>
+                    <li>{site.city}, </li>
+                    <li>{site.state}</li>
+                    <br></br>
+                </ul>
+                    <Link to="/site" element={<TouristPlace />} siteData = {site}>Visit Site </Link>
+            </div>
+        </div>
+    
+    })
+
+
     return (
       <div className="PopPlaces">
         <section id = "popular" class = "py-4">
             <div class = "title-wrap">
-                <span class = "sm-title">know about some cool destination</span>
+                <span class = "sm-title">Know about some cool destinations</span>
                 <h2 class = "lg-title">Popular Places</h2>
             </div>
-
             <div class = "popular-row">
-                <div class = "popular-item shadow">
-                    <img src = "images/popular-1.jpg" alt = "" />
-                    <div><i class = "fas fa-map-marker-alt"></i>
-                        <span>Eiffel Tower, Paris</span>
-                        <ul class = "rating flex">
-                            <li><i class = "fas fa-star"></i></li>
-                            <li><i class = "fas fa-star"></i></li>
-                            <li><i class = "fas fa-star"></i></li>
-                            <li><i class = "fas fa-star"></i></li>
-                            <li><i class = "fas fa-star-half-alt"></i></li>
-                            <li>&nbsp;400 reviews</li>
-                        </ul>
-                        <p class = "text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, quia!</p>
-                    </div>
-                </div>
+                {displaySite}
             </div>
+            
+
         </section>
       </div>
     );
